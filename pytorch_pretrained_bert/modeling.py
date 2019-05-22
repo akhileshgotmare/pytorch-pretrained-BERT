cc683ce764e1de9e6a29c60c71621478863c5e57
Akhilesh Gotmare <akhilesh.gotmare@epfl.ch>
2019-05-22 16:24:59 +0800

    Update modeling.py to include NABERT
    Towards the BERT equivalent of NAQANet from https://arxiv.org/pdf/1903.00161.pdf

--- pytorch_pretrained_bert/modeling.py
+++ pytorch_pretrained_bert/modeling.py
@@ -1223,3 +1223,117 @@ class BertForQuestionAnswering(BertPreTrainedModel):
             return total_loss
         else:
             return start_logits, end_logits
+
+        
+        
+        
+class NABertForDROP(BertPreTrainedModel):
+    """Numerically augmented BERT model for DROP - modified from BertForQuestionAnswering class above.
+    
+    NABERT adapts the 4 answer type layers from AI2's DROP baseline (NAQANet) - https://arxiv.org/pdf/1903.00161.pdf
+
+    Params:
+        `config`: a BertConfig class instance with the configuration to build a new model.
+
+    Inputs:
+        `input_ids`: a torch.LongTensor of shape [batch_size, sequence_length]
+            with the word token indices in the vocabulary(see the tokens preprocessing logic in the scripts
+            `extract_features.py`, `run_classifier.py` and `run_squad.py`)
+        `token_type_ids`: an optional torch.LongTensor of shape [batch_size, sequence_length] with the token
+            types indices selected in [0, 1]. Type 0 corresponds to a `sentence A` and type 1 corresponds to
+            a `sentence B` token (see BERT paper for more details).
+        `attention_mask`: an optional torch.LongTensor of shape [batch_size, sequence_length] with indices
+            selected in [0, 1]. It's a mask to be used if the input sequence length is smaller than the max
+            input sequence length in the current batch. It's the mask that we typically use for attention when
+            a batch has varying length sentences.
+        `start_positions`: position of the first token for the labeled span: torch.LongTensor of shape [batch_size].
+            Positions are clamped to the length of the sequence and position outside of the sequence are not taken
+            into account for computing the loss.
+        `end_positions`: position of the last token for the labeled span: torch.LongTensor of shape [batch_size].
+            Positions are clamped to the length of the sequence and position outside of the sequence are not taken
+            into account for computing the loss.
+
+    Outputs:
+        if `start_positions` and `end_positions` are not `None`:
+            Outputs the total_loss which is the sum of the CrossEntropy loss for the start and end token positions.
+        if `start_positions` or `end_positions` is `None`:
+            Outputs a tuple of start_logits, end_logits which are the logits respectively for the start and end
+            position tokens of shape [batch_size, sequence_length].
+
+    Example usage:
+    ```python
+    # Already been converted into WordPiece token ids
+    input_ids = torch.LongTensor([[31, 51, 99], [15, 5, 0]])
+    input_mask = torch.LongTensor([[1, 1, 1], [1, 1, 0]])
+    token_type_ids = torch.LongTensor([[0, 0, 1], [0, 1, 0]])
+
+    config = BertConfig(vocab_size_or_config_json_file=32000, hidden_size=768,
+        num_hidden_layers=12, num_attention_heads=12, intermediate_size=3072)
+
+    model = BertForQuestionAnswering(config)
+    start_logits, end_logits = model(input_ids, token_type_ids, input_mask)
+    ```
+    """
+    def __init__(self, config):
+        super(BertForQuestionAnswering, self).__init__(config)
+        self.bert = BertModel(config)
+        # TODO check with Google if it's normal there is no dropout on the token classifier of SQuAD in the TF version
+        # self.dropout = nn.Dropout(config.hidden_dropout_prob)
+        self.qa_outputs = nn.Linear(config.hidden_size, 2)
+        self.apply(self.init_bert_weights)
+        
+        self.w_p = nn.Linear() #TODO: figure out dimensions here!
+        self.question_span_start = nn.Linear() #TODO: figure out dimensions here!
+        self.question_span_end = nn.Linear() #TODO: figure out dimensions here!
+        
+        self.count = nn.Linear() #TODO: figure out dimensions here!
+        
+        self.answer_type = nn.Linear() #TODO: figure out dimensions here!
+        
+        self.splitpassagequestion = #TODO: Some pre-processing to split sequence_output to psg and question representation
+        
+
+    def forward(self, input_ids, token_type_ids=None, attention_mask=None, start_positions=None, end_positions=None):
+        
+        #sequence_output here would be equivalent to [P;Q] from NAQANet
+        sequence_output, _ = self.bert(input_ids, token_type_ids, attention_mask, output_all_encoded_layers=False)
+        
+        logits = self.qa_outputs(sequence_output)
+        start_logits, end_logits = logits.split(1, dim=-1)
+        start_logits = start_logits.squeeze(-1)
+        end_logits = end_logits.squeeze(-1)
+        
+        passage_represn, question_represn = self.splitpassagequestion(sequence_output)
+        
+        h_p = torch.mm(nn.Softmax(self.w_p(passage_represn)),(passage_represn))
+        h_p_repeated = h_p.unsqueeze(2).expand(100, 700, 28) #see how to repeat here - https://discuss.pytorch.org/t/how-to-repeat-a-vector-batch-wise/1485/2
+        
+        h_q = torch.mm(nn.Softmax(self.w_q(question_represn)),(question_represn))
+        
+        
+        p_q_start = nn.Softmax(self.question_span_start(torch.cat(question_represn,h_p_repeated)))
+        p_q_end   = nn.Softmax(self.question_span_end(torch.cat(question_represn,h_p_repeated)))
+
+        p_count = nn.Softmax(self.count(h_p))
+        
+        p_answer_type = nn.Softmax(self.answer_type(torch.cat(h_p,h_q)))
+        
+        if start_positions is not None and end_positions is not None:
+            # If we are on multi-GPU, split add a dimension
+            if len(start_positions.size()) > 1:
+                start_positions = start_positions.squeeze(-1)
+            if len(end_positions.size()) > 1:
+                end_positions = end_positions.squeeze(-1)
+            # sometimes the start/end positions are outside our model inputs, we ignore these terms
+            ignored_index = start_logits.size(1)
+            start_positions.clamp_(0, ignored_index)
+            end_positions.clamp_(0, ignored_index)
+
+            loss_fct = CrossEntropyLoss(ignore_index=ignored_index)
+            start_loss = loss_fct(start_logits, start_positions)
+            end_loss = loss_fct(end_logits, end_positions)
+            total_loss = (start_loss + end_loss) / 2
+            return total_loss
+        else:
+            return start_logits, end_logits
+      
